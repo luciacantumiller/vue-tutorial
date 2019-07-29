4869db758526d5be284f3fd6477e2f50af0f7c9c
Lucía Cantú-Miller <luciacan2miller@gmail.com>
2019-07-29 15:02:46 -0500

    last

--- resources/js/components/LoadersAndAnimations.vue
+++ resources/js/components/LoadersAndAnimations.vue
@@ -1,7 +1,20 @@
 <template>
-    <h1 class="font-normal text-3xl text-gray-700 leading-none">
-        Loaders and Animations
-    </h1>
+    <div>
+        <h1 class="font-normal text-3xl text-gray-700 leading-none">
+            Loaders and Animations
+        </h1>
+
+        <p class="mb-10 text-grey-700">
+            This spinner uses AirBnb's 
+            <a href="http://airbnb.io/lottie/#/web" class="text-blue font-bold">Lottie</a>
+            library to render its animation. You may download the necessary assets below.
+        </p>
+
+        <div class="border border-grey-200 rounder lg flex items-center justify-center mb-8">
+            <div ref="lottie" class="w-64" style="height: 457px"></div>
+
+        </div>
+    </div>
 </template>
 
 <script>
